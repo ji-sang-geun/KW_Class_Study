5d3d4c715013e260b4b11baf3b4491f6ceca388e
unknown <suki5038@naver.com>
2021-02-02 18:28:52 +0900

    포인터 분석하기

--- Programming_C_2019/2019_0917_C_1.c
+++ Programming_C_2019/2019_0917_C_1.c
@@ -1,28 +1,14 @@
-int main(void) // 메인 함수 정의
+#include <stdio.h>
+
+int main(void)
 {
-	char ch, *cp; // 문자형 ch와 문자형 포인터 cp정의
-	int num, *ip; // 정수형 num과  정수형 포인터 ip정의
-	float f_num, *fp; // float형 f_num과 float형 포인터 fp정의
-	double d_num, *dp; // double형 d_num과 double형 포인터 dp정의
+	int a[] = {9,8,7,6};
+	int *p = a;
 
-	ch = 'A'; num = 100; // ch를 A에 대입하여 초기화, num에 100을 대입하여 초기화
-	f_num = 5.6; d_num = 17.5; // f_num에 5.6을 대입하여 초기화, d_num에도 17.5를 대입하여 초기화
-	
-	cp = &ch; // cp에 ch의 주소값을 대입
-	ip = &num; // ip에 num의 주소값을 대입
-	fp = &f_num; // fp에 f_num의 주소값을 대입
-	dp = &d_num; // dp에 d_num의 주소값을 대입
+	printf("%d ", *p++); // *p = &a[0]이고 *(p++)랑 같으므로 9가 출력
+	printf("%d ", (*p)++); // (*p)++이니 (*p)에서 주소언급이 끝났으므로 8출력
+	printf("%d ", *++p); // *++p는 *(++p)이므로 7 출력
+	printf("%d ", ++*p); //++(*p)이므로 1이 더해지고  8 출력
 
-	printf("1 증가하기 전 변수와 포인터 변수. \n"); 
-	printf("변수들의 값 : %5c %5d %10.2f %10.2lf \n", ch, num, f_num, d_num); // 증가하기 전 변수들의 값을 출력 
-	printf("포인터 변수에 저장된 주소 : %p %p %p %p \n\n", cp, ip, fp, dp);  // 증가하기 전 변수들의 주소값을 출력
-
-	ch++; num++; f_num++; d_num++; // 후위 연산자로 ch, num, f_num, d_num 1씩 증가
-	cp++; ip++; fp++; dp++; // 후위 연산자로 cp, ip, fp, dp 1씩 증가
-
-	printf("1 증가한 후 변수와 포인터 변수. \n");
-	printf("변수들의 값 : %5c %5d %10.2f, %10.2lf \n", ch, num, f_num, d_num); // 증가한 후 변수들의 값을 출력  ( 증가하기 전 변수들에 비해 값이 1씩 증가하여 B, 101, 6.60, 18.50 출력)
-	printf("포인터 변수에 저장된 주소 : %p %p %p %p \n\n", cp, ip, fp, dp); // 증가한 후 변수들의 주소값을 출력 ( 증가하기 전 변수들의 주소값에 비해 1, 4, 4, 8 바이트씩 증가 ) 
-
-	return 0; // 프로그램 종료
+	return 0;
 }
