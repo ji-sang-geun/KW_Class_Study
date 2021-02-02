859f51d2d0cdd077efeec983210acf889fb91c89
unknown <suki5038@naver.com>
2021-02-02 19:09:38 +0900

    1부터 n까지의 합 재귀함수

--- Programming_C_2019/2019_0508_C_1.c
+++ Programming_C_2019/2019_0508_C_1.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+int n = 0; // 전역변수 n 제시
 
-void count(void);
+int sumFunc(int count) //sumFunc함수 정의
+{
+	if (count > 0) //count가 0보다 크면 그 밑 함수 작동 하기
+	{
+		return count + sumFunc(count - 1); // count에 sumFunc(count - 1) 더하는 반환값 가짐
+	}
+	else // 만약 count가 0이면 그 밑 함수 되게끔 하기
+	{
+		return 0;//반환값 0가짐
+	}
+}
 
 int main(void)
 {
-	int i;
-	for (i = 1; i <= 3; i++)
-		count();
-	return 0;
+	
+	printf("합게를 원하는 정수를 입력하시오 : ");
+	scanf("%d", &n);
+	printf("1부터 %d까지의 합: %d", n, sumFunc(n)); //sumFunc함수 출력
+
+	return 0; // 프로그램 종료
 }
 
-void count(void)
-{
-	int acnt = 0;
-	static stcnt = 0;
-	acnt += 1;
-	stcnt += 1;
-	printf("auto count = %d , static count = %d \n", acnt, stcnt);
-}
