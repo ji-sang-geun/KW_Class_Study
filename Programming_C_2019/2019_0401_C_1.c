a22addd820f68a4ded7368efe4e4389a1c8caf2e
unknown <suki5038@naver.com>
2021-02-02 18:49:38 +0900

    #include <stdio.h>

---
+++ Programming_C_2019/2019_0401_C_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+int main(void)
+{
+	printf("F \n"); //직각삼각형 
+	printf("F");
+	printf(" F \n");
+	printf("F");
+	printf(" F");
+	printf(" F \n");
+	printf("F");
+	printf(" F");
+	printf(" F");
+	printf(" F \n");
+	printf("F");
+	printf(" F");
+	printf(" F");
+	printf(" F");
+	printf(" F \n");
+
+	printf("%5d \n", 5); // 정삼각형 %5d는 필드폭 5칸 확보
+	printf("%4d %1d \n", 5,5);
+	printf("%3d %1d %1d \n", 5,5,5);
+	printf("%2d %1d %1d %1d \n", 5,5,5,5);
+	printf("%1d %1d %1d %1d %1d \n", 5,5,5,5,5);
+
+	return 0;
+}
